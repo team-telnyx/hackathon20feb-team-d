044d56ddd359b6cec6f1875a81f36c97ac18090f
somemothersson <stevemalito@gmail.com>
2020-02-26 17:00:01 -0600

    prod

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // Init Socket.IO
 io.on("connection", socket => {
