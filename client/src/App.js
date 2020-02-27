1944fb18396129506f7e7f6e256171cf1b20a28c
Lucas Rosa <lucas@telnyx.com>
2020-02-26 21:59:01 -0300

    add suggestion alert to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import { withAlert } from "react-alert";
 import logo from "./logo.svg";
 // NPM Module used for mobile device type DOM https://github.com/Wolox/react-chat-widget
 import { Widget, addResponseMessage, toggleWidget } from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
+import { positions, Provider } from "react-alert";
 // AXIOS used to POST new SMS to Telnyx API
 import axios from "axios";
 // Socket.io client for React
 import io from "socket.io-client";
+
+const options = {
+  timeout: 25000,
+  position: positions.TOP_RIGHT
+};
 
 class App extends Component {
   constructor() {
@@ -43,21 +50,37 @@ class App extends Component {
     // Listening for events from backend endpoint which recieves webhooks from Telnyx
     socket.on("sms msg", msg => {
       // react-chat-widget method employed to post recieved SMS to DOM
-      addResponseMessage(msg.msgBody);
+      addResponseMessage(msg);
+
       console.log(`SMS Received: ${msg.msgBody} from: ${msg.msgFrom}`);
+      this.props.alert.show(
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>
+            ({msg.msgFrom}) {msg.msgBody}
+          </div>
+          <div style={{ display: "flex" }}>
+            <span>{msg.answer}</span>
+            <button onClick={() => handleNewUserMessage(msg.answer)}>
+              send
+            </button>
+          </div>
+        </div>
+      );
     });
 
     return (
-      <div className="App">
-        <Widget
-          handleNewUserMessage={this.handleNewUserMessage}
-          profileAvatar={logo}
-          title="Telnyx SMS P2P Demo"
-          subtitle="Let's Chat"
-        />
-      </div>
+      <Provider template={AlertTemplate} {...options}>
+        <div className="App">
+          <Widget
+            handleNewUserMessage={this.handleNewUserMessage}
+            profileAvatar={logo}
+            title="Telnyx SMS P2P Demo"
+            subtitle="Let's Chat"
+          />
+        </div>
+      </Provider>
     );
   }
 }
 
-export default App;
+export default withAlert()(App);
